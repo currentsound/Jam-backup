9f5cbf08bcbf6b4393df1a67aadc3982747c14ab
Christoph Witzany <christoph@web.crofting.com>
2023-10-21 11:26:32 +0200

    Improve typing

--- ui/jam-core.ts
+++ ui/jam-core.ts
@@ -131,7 +131,7 @@ function createApi<T extends StateType>(
 
     startRecording: () => dispatch('start-recording'),
     stopRecording: () => dispatch('stop-recording'),
-    getRecordingsDownloadLink: roomId => recordingsDownloadLink(state, roomId),
+    getRecordingsDownloadLink: (roomId: string) => recordingsDownloadLink(state, roomId),
 
     startServerRecording: () => dispatch(actions.START_SERVER_RECORDING),
     stopServerRecording: () => dispatch(actions.STOP_SERVER_RECORDING),
